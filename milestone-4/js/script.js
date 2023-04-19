e68f066172732adea95b768f16cdc451cead8081
CosimoTramonte <cosimotramonte7@gmail.com>
2023-04-19 13:06:16 +0200

    add milestone 4

---
+++ milestone-4/js/script.js
@@ -0,0 +1,76 @@
+const { createApp } = Vue;
+
+const dt = luxon.DateTime
+
+createApp({
+
+    data(){
+
+        return{
+            contacts,
+            counter: 0,
+            newMessageToSent: "",
+            clock: null,
+            filterChat: "",
+        }
+    },
+
+    methods:{
+        changeProfile(index){
+            this.counter = index
+        },
+
+        addMessage(){
+
+            this.printClock()
+
+            const newMessage = {
+                date: this.clock,
+                message: this.newMessageToSent,
+                status: 'sent'
+            }
+
+            
+            this.contacts[this.counter].messages.push(newMessage);
+            this.autoMessage()
+            this.newMessageToSent=""  
+        },
+
+        autoMessage(){
+
+            setTimeout(() =>{
+
+                this.printClock()
+
+                const newAutoMessage = {
+                    date: this.clock,
+                    message: "ok",
+                    status: 'received'
+                }
+                this.contacts[this.counter].messages.push(newAutoMessage);
+
+            }, 1000)
+            
+        },
+
+        printClock(){
+            this.clock = dt.now().setLocale('it').toLocaleString(dt.DATETIME_SHORT_WITH_SECONDS);
+            console.log(this.clock);
+        },
+
+    },
+
+    computed: {
+
+        listFilter(){
+
+            this.contacts.forEach((contact) => { 
+                contact.visible = contact.name.toLowerCase().includes(this.filterChat.toLowerCase())
+            });
+
+            return this.contacts.visible
+        }
+    }
+
+
+}).mount('#app');
